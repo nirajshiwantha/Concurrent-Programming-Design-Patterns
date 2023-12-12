f3b3bb4d05689ab95c5fa18c057ed802e8ea3b91
niraj_ranasinghe <niraj.ranasinghe@covalentworld.com>
2023-12-12 23:27:25 +0530

    producer-consumer-pattern updated

--- DesignPatterns/Producer-Consumer-Pattern/Producer-Consumer-Pattern-sample-2.cs
+++ DesignPatterns/Producer-Consumer-Pattern/Producer-Consumer-Pattern-sample-2.cs
@@ -6,31 +6,31 @@ public class Producer_Consumer_Pattern_sample_2
     static object lockObject = new object();
     static bool isLoggingCompleted = false;
 
-    static async Task Main()
-    {
-        // Start a logger consumer asynchronously
-        Task loggerTask = Task.Run(() => Logger());
+    //static async Task Main()
+    //{
+    //    // Start a logger consumer asynchronously
+    //    Task loggerTask = Task.Run(() => Logger());
 
-        // Start multiple producers asynchronously
-        List<Task> producerTasks = new List<Task>();
-        for (int i = 1; i <= 3; i++)
-        {
-            int producerId = i;
-            producerTasks.Add(Task.Run(() => Producer(producerId)));
-        }
+    //    // Start multiple producers asynchronously
+    //    List<Task> producerTasks = new List<Task>();
+    //    for (int i = 1; i <= 3; i++)
+    //    {
+    //        int producerId = i;
+    //        producerTasks.Add(Task.Run(() => Producer(producerId)));
+    //    }
 
-        // Wait for all producer tasks to complete
-        await Task.WhenAll(producerTasks);
+    //    // Wait for all producer tasks to complete
+    //    await Task.WhenAll(producerTasks);
 
-        // Signal that logging is completed
-        lock (lockObject)
-        {
-            isLoggingCompleted = true;
-        }
+    //    // Signal that logging is completed
+    //    lock (lockObject)
+    //    {
+    //        isLoggingCompleted = true;
+    //    }
 
-        // Wait for the logger to complete
-        await loggerTask;
-    }
+    //    // Wait for the logger to complete
+    //    await loggerTask;
+    //}
 
     static void Producer(int producerId)
     {
--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
 ﻿class Program
 {
-    //static void Main()
-    //{
-    //    Console.WriteLine("Uncomment the Main Method in each class to run the code!!!");
-    //}
-
+    static void Main()
+    {
+        Console.WriteLine("Uncomment the Main Method in each class and run the code see the results!!!");
+    }
 }
